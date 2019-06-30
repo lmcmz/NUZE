69b0555ffe570221f368b3ac367212131dc50cae
Michael <huangjkau@gmail.com>
2019-06-30 16:36:09 +1000

    finished jwt auth

--- Server/src/main/java/xyz/nuze/services/ServiceImp/ClientServiceImp.java
+++ Server/src/main/java/xyz/nuze/services/ServiceImp/ClientServiceImp.java
@@ -1,6 +1,7 @@
 package xyz.nuze.services.ServiceImp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import xyz.nuze.error.BusinessException;
 import xyz.nuze.mapper.ClientMapper;
 import xyz.nuze.model.Client;
@@ -10,6 +11,8 @@ import xyz.nuze.services.ClientService;
  * @CreatyBy Michael
  * @Date 2019-06-30 16:22
  */
+
+@Service
 public class ClientServiceImp implements ClientService {
 
     @Autowired
