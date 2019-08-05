a86a1eba66487b9c36474be4c4dafe3411e69975
lmcmz <lmcmze@gmail.com>
2019-08-05 22:02:57 +1000

    Add local storage

--- Web/src/redux/index.js
+++ Web/src/redux/index.js
@@ -1,9 +1,19 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import userReducer  from './user'
+import { saveState, loadState} from './storage/localStorage'
+import throttle from 'lodash.throttle';
 
 const reducer = combineReducers({
     user:userReducer,
 })
-const store = createStore(reducer, applyMiddleware(thunk))
+
+const presistedState = loadState();
+const store = createStore(reducer, presistedState, applyMiddleware(thunk))
+store.subscribe(throttle(() => {
+    saveState({
+        user: store.getState().user
+    });
+}, 1000));
+
 export default store
